86e1180569b45844285cf97c13ecc5c3a6cc9b3b
Sarcoma <sarcoma@live.co.uk>
2016-05-20 09:37:15 +0100

    All side blocks are now the same dimensions

--- src/js/main.js
+++ src/js/main.js
@@ -6,12 +6,9 @@ var winWidth = $(window).width() - 20, // -20 Removes Scroll Bar
     newCornerSize,
     blockWidth = 200,
     blockHeight = 100,
-    maxWidth = 300,
-    minWidth = 100,
     margin = 20,
-    newMargin = 20,
+    newMargin,
     availableWidth = winWidth - (margin * 2) - (cornerSize * 2),
-    availableHeight = winHeight - (margin * 2) - (cornerSize * 2),
     block,
     roughHSegments,
     diffH,
@@ -35,43 +32,44 @@ actualHSeg = Math.ceil(roughHSegments);
 diffH = roughHSegments / actualHSeg;
 newWidth = blockWidth*diffH;
 newHeight = blockHeight*diffH;
+
 newCornerSize = cornerSize*diffH;
 newMargin = margin + Math.ceil((cornerSize - newCornerSize));
 
-console.log(diffH);
+availableHeight = winHeight - (newMargin * 2) - (newCornerSize * 2);
+roughVSegments = availableHeight / newWidth;
+actualVSeg = Math.floor(roughVSegments);
+diffV = availableHeight - Math.ceil(newWidth * actualVSeg);
+var newVMargin = newMargin + (diffV/2);
+var newVOffSet = newVMargin + newCornerSize;
 
 $(".corner").css({
     "width": newCornerSize,
     "height": newCornerSize
 });
 
-$(".top").css("top", newMargin+"px");
+$(".top").css("top", newVMargin+"px");
 $(".right").css("right", newMargin+"px");
-$(".bottom").css("bottom", newMargin+"px");
+$(".bottom").css("bottom", newVMargin+"px");
 $(".left").css("left", newMargin+"px");
+
+// ToDo: rotate blocks CSS Transform?
 
 for (var i = 0; i < actualHSeg; i++) {
     move = offSet + (i * newWidth);
-    block = createBlock("width:"+newWidth+"px; height:"+newHeight+"px; top:"+newMargin+"px; left:"+ move + "px;");
+    block = createBlock("width:"+newWidth+"px; height:"+newHeight+"px; top:"+newVMargin+"px; left:"+ move + "px;");
     screen.append(block);
-    block = createBlock("width:"+newWidth+"px; height:"+newHeight+"px; bottom:"+newMargin+"px; left:"+ move + "px;");
+    block = createBlock("width:"+newWidth+"px; height:"+newHeight+"px; bottom:"+newVMargin+"px; left:"+ move + "px;");
     screen.append(block);
 }
 
-roughVSegments = availableHeight / blockWidth;
-actualVSeg = Math.ceil(roughVSegments);
-diffV = roughVSegments / actualVSeg;
-newWidth = blockWidth*diffV;
-newHeight = blockHeight*diffV;
-
 for (i = 0; i < actualVSeg; i++) {
-    move = offSet + (i * newWidth);
+    move = newVOffSet + (i * newWidth);
     block = createBlock("width:"+newHeight+"px; height:"+newWidth+"px; left:"+newMargin+"px; top:"+ move + "px;");
     screen.append(block);
     block = createBlock("width:"+newHeight+"px; height:"+newWidth+"px; right:"+newMargin+"px; top:"+ move + "px;");
     screen.append(block);
 }
-
 
 function createBlock(styles) {
     return "<div class=\"block\" style=\""+styles+"\"></div>"
